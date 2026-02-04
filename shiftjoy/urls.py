f533d37842d353194f481c24ba00ec54ae7173d1
BobBjorklund <bjorklr1@tcnj.edu>
2026-02-04 06:06:17 -0500

    fully moved to redis with 8 minute health check to keep render live

--- shiftjoy/urls.py
+++ shiftjoy/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from bingo.views import healthz
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bingo/', include('bingo.urls')),
+    path('healthz', healthz, name="healthz"),
 ]
